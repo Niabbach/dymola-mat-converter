b339d6817b059667efc14ce94cd0be579ccc474d
niangai231 <channel.nianga-ibabassombo@etu.unicaen.fr>
2026-01-01 07:59:23 +0100

    Ajout des Json timeseries

--- src/exporter.py
+++ src/exporter.py
@@ -9,5 +9,19 @@ def export_all(df, static, name):
     df.to_csv(f"data/csv/{name}.csv", index=False)
     df.to_parquet(f"data/parquet/{name}.parquet", index=False)
 
+    # paramètres statiques
     with open(f"data/json/{name}_static.json", "w") as f:
         json.dump(static, f, indent=2)
+
+    # résultats temporels
+    timeseries_json = {
+        "time": df["time"].tolist(),
+        "signals": {
+            col: df[col].tolist()
+            for col in df.columns if col != "time"
+        }
+    }
+
+    with open(f"data/json/{name}_timeseries.json", "w") as f:
+        json.dump(timeseries_json, f, indent=2)
+
